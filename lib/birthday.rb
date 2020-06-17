58fbd1ef0e12e45ed95e602e1a6db939fb08a378
devWRM <mcwen@comcast.net>
2020-06-16 22:53:38 -0400

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,17 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  messages = []
+  birthday_kids.each do |kid, age|
+    
+      # if age 
+        puts "Happy Birthday #{kid}! You are now #{age} years old!"
+
+      # else
+      #   "You're too old for this."
+      # end
+  end
+    messages
 end
 
 
